e316fdf7bf229f7164ad0fafcda3e2b4185b3c26
LawrenciaDzifa <dzifa01ahiadeke@gmail.com>
2023-02-16 19:47:39 +0000

    added packaged animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -25,7 +26,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     controller.forward();
     controller.addListener(() {
       setState(() {});
-      print(controller.value);
+      // print(controller.value);
     },);
   }
 
@@ -48,13 +49,14 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                     height: 60.0,
                   ),
                 ),
-                Text(
+                AnimatedTextKit(animatedTexts: [ TypewriterAnimatedText(
                   'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
+                  textStyle: TextStyle(
+                    fontSize: 43.0,
                     fontWeight: FontWeight.w900,
                   ),
-                ),
+                ),])
+               
               ],
             ),
             SizedBox(
